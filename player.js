f17f3a153f033991b0e1e3989d0d24a002b18d4f
Mariani Giacomo <mariang1@t31300-lr138.org.aalto.fi>
2020-04-23 20:11:50 +0300

    WIP

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 let $ = window.$;
-let stepcount = 0
+let stepcount = -1
+let showClicks = false;
 
 initialize();
 
@@ -7,8 +8,8 @@ async function initialize() {
   try {
     let submission = await getSubmission();
     if(submission && Object.keys(submission).length > 0){
-      initiateAnimation(submission);
-      setKeyboardListeners();
+      initiateSlideShow(submission);
+      initializeAnimationButtonsListeners(submission);
     } else {
       console.warn('No animation data received')
     }
@@ -30,15 +31,12 @@ async function getSubmission() {
 }
 
 
-function initiateAnimation(submission) {
-  let showClicks = true;
-  let gradableSteps = submission.animation.filter(step => step.type === 'gradeable-step');
-  let clickSteps = submission.animation.filter(step => step.type === 'click');
-  let animationSteps = showClicks? clickSteps : gradableSteps;
+function initiateSlideShow(submission) {
+  let animationSteps = getAnimationSteps(submission);
   try {
     $('#animation-container')[0].innerHTML = submission.initialState.animationDOM;
     $('#step-forward').on('click', () => {
-      if (stepcount +1 < animationSteps.length -1) {
+      if (stepcount +1 < animationSteps.length) {
         stepcount ++;
         $('#animation-container')[0].innerHTML = animationSteps[stepcount].animationDOM;
       } else {
@@ -46,11 +44,11 @@ function initiateAnimation(submission) {
       }
     });
     $('#to-beginning').on('click', () => {
-      stepcount = 0;
+      stepcount = -1;
       $('#animation-container')[0].innerHTML = submission.initialState.animationDOM;
     });
     $('#step-backward').on('click', () => {
-      if (stepcount -1 > 0) {
+      if (stepcount > 0) {
         stepcount --;
         $('#animation-container')[0].innerHTML = animationSteps[stepcount].animationDOM;
       } else {
@@ -67,6 +65,12 @@ function initiateAnimation(submission) {
   }
 }
 
+function getAnimationSteps(submission) {
+  let gradableSteps = submission.animation.filter(step => step.type === 'gradeable-step');
+  let clickSteps = submission.animation.filter(step => step.type === 'click');
+  return showClicks? clickSteps : gradableSteps;
+}
+
 function exportAnimation(submission) {
   const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(submission));
   newWindow = window.open(dataStr);
@@ -76,49 +80,65 @@ function exportAnimation(submission) {
   // dlAnchorElem.click();
 }
 
-function setKeyboardListeners() {
-  $('#animation-container')[0].click();
-  $("#play-button").on('click', startAutoAnimation)
-  document.onkeydown = function(event) {
-    switch (event.keyCode) {
-      case 37:
-        $('#step-backward')[0].click()
-        break;
-      case 38:
-        $('#to-beginning')[0].click()
-        break;
-      case 39:
-        $('#step-forward')[0].click()
-        break;
-      case 40:
-        $('#to-end')[0].click()
-        break;
+function handlePlayAnimation(animationSteps) {
+  function playAnimation(steps) {
+    let interval = setInterval(stepForward, 1000);
+    function stepForward() {
+      if(stepcount +1 < animationSteps.length) {
+        stepcount ++;
+        $('#animation-container')[0].innerHTML = animationSteps[stepcount].animationDOM;
+      } else {
+        $('#animation-container')[0].innerHTML = '<h3>Ended</h3>'
+        clearAndResetInterval(interval, $("#play-button"), () => playAnimation(animationSteps));
+        stepcount = -1;
+      }
     }
   }
-}
-
-function startAutoAnimation() {
-  let animator = startAnimator()
-  $("#play-button").off('click', startAutoAnimation)
-  $('#step-forward').click()
-  $("#stop-button").on('click', () => {
-    clearInterval(animator)
-    $('#to-beginning').click();
-    $("#play-button").on('click', startAutoAnimation)
-  })
+  $("#play-button").off('click', playAnimation);
+  if(stepcount === -1) {
+    $('#animation-container')[0].innerHTML = animationSteps[stepcount].animationDOM;
+    setTimeout(playAnimation(animationSteps));
+  } else {
+    playAnimation(animationSteps)
+  }
   $("#pause-button").on('click', () => {
-    clearInterval(animator)
-    $("#play-button").on('click', startAutoAnimation)
-  })
+      clearAndResetInterval(interval, $("#play-button"), () => handlePlayAnimation(animationSteps));
+  });
 }
 
-function startAnimator() {
-  return setInterval(timedAction, 1000);
+
+
+function clearAndResetInterval(interval, $element, action) {
+  clearInterval(interval)
+  $element.on('click', action)
 }
 
-function timedAction() {
-  $('#step-forward').click();
+function initializeAnimationButtonsListeners(submission) {
+  let animationSteps = getAnimationSteps(submission);
+  $("#play-button").on('click', () => handlePlayAnimation(animationSteps))
 }
+// function startAutoAnimation() {
+//   let animator = startAnimator()
+//   $("#play-button").off('click', startAutoAnimation)
+//   $('#step-forward').click()
+//   $("#stop-button").on('click', () => {
+//     clearInterval(animator)
+//     $('#to-beginning').click();
+//     $("#play-button").on('click', startAutoAnimation)
+//   })
+//   $("#pause-button").on('click', () => {
+//     clearInterval(animator)
+//     $("#play-button").on('click', startAutoAnimation)
+//   })
+// }
+//
+// function startAnimator() {
+//   return setInterval(timedAction, 1000);
+// }
+//
+// function timedAction() {
+//   $('#step-forward').click();
+// }
 
 module.exports = {
   initialize
